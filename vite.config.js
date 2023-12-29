44e0886ad388aed8b7318ad5224abc35609546b9
Gokul Lekhwar <lekhwargokul84@gmail.com>
2023-12-29 20:06:38 +0530

    updated mobile view

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ViteGHPages from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React-Calculator/",
+  base: "/Calculator/",
 });
